37b379c96dde19776c37143fd605085bc52bc77b
Ryan Greenup <exogenesis@protonmail.com>
2020-08-14 20:48:41 +1000

    Finished BigData MongoDB Tutorial

--- BigData/Practicals/.auctex-auto/04-MongoDB.el
+++ BigData/Practicals/.auctex-auto/04-MongoDB.el
@@ -5,14 +5,17 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -34,20 +37,13 @@
     "minted"
     "/home/ryan/Dropbox/profiles/Templates/LaTeX/ScreenStyle")
    (LaTeX-add-labels
-    "sec:orgf97b54d"
-    "sec:orgc89a261"
-    "sec:org533f114"
-    "sec:orgacd83d9"
-    "sec:org834a7ff"
-    "sec:orgf92bb53"
-    "sec:orgde5a1f2"
-    "sec:org137624a"
-    "sec:org3edf32c"
-    "sec:org5b0daf1"
-    "sec:orge20ee8d"
-    "sec:orgd7278cc"
-    "sec:org51e78c9"
-    "sec:org4cbf32c"
-    "sec:org15284f9"))
+    "sec:org6fad599"
+    "sec:org4b889ba"
+    "sec:orga8ff286"
+    "sec:org2e247c6"
+    "sec:org4e179ed"
+    "sec:org0e9a631")
+   (LaTeX-add-environments
+    '("sol" LaTeX-env-args ["argument"] 0)))
  :latex)
 
